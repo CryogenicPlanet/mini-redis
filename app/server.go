6a2aeac01e3b39eeb4c91afc5725a11cf18c9e0b
Rahul Tarak <cryogenicplanet@gmail.com>
2022-02-13 05:25:40 +0530

    🚧 Recieve basic message

--- app/server.go
+++ app/server.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"strings"
+
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		message := scanner.Text()
+		fmt.Println("Message Received:", message)
+		newMessage := strings.ToUpper(message)
+		conn.Write([]byte(newMessage + "\n"))
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("error:", err)
+	}
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -18,9 +37,13 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Calling handleConnection")
+		go handleConnection(conn)
 	}
+
 }
