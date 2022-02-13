4ee35e9819fe8ef779ca944a4ddff37952a6225e
Rahul Tarak <cryogenicplanet@gmail.com>
2022-02-13 07:09:41 +0530

    🚧 multiple pongs

--- app/server.go
+++ app/server.go
@@ -4,21 +4,34 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"strconv"
 	"strings"
+	"sync"
 
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
 
+var responses = make(chan string, 10)
+var responseWg sync.WaitGroup
+
 func toRESPString(message string) string {
 	return "+" + message + "\r\n"
 }
 
-// Expect every message to be in RESP and be an Array of bulk strings
-func handleMessage(message string) (string, error) {
+func writeResponse(conn net.Conn) {
+	for response := range responses {
+		fmt.Println("Response", response)
+		conn.Write([]byte(response))
+		responseWg.Done()
+	}
+}
 
-	splits := strings.Split(message, "\r\n")
+// Expect every message to be in RESP and be an Array of bulk strings
+func handleMessage(message string) error {
+
+	splits := strings.Split(message, `\r\n`)
 
 	fmt.Println("Splits", splits)
 
@@ -26,37 +39,51 @@ func handleMessage(message string) (string, error) {
 
 	if firstSplit[0] != '*' {
 		// Not an valid RESP message
-		return "", fmt.Errorf("Not a valid RESP message")
+		return fmt.Errorf("Not a valid RESP message")
 	}
 
-	arrSize := int(firstSplit[1])
+	arrSize, err := strconv.Atoi(string(firstSplit[1]))
 
-	if arrSize == 0 {
-		return "", fmt.Errorf("Null RESP Array")
+	fmt.Println("Arr size", arrSize, firstSplit)
+
+	if arrSize == 0 || err != nil {
+		return fmt.Errorf("Null RESP Array")
 	}
 
-	return toRESPString("PONG"), nil
+	if arrSize > 1 {
+		// Have to loop over messages
+		for i := 0; i < arrSize; i++ {
+			fmt.Println("Responded to X PING", i)
+			responses <- toRESPString("PONG")
+			responseWg.Add(1)
+		}
+		return nil
+	}
+
+	responses <- toRESPString("PONG")
+	responseWg.Add(1)
+
+	return nil
 }
 
 func handleConnection(conn net.Conn) {
 	fmt.Println("Waiting for connections ...")
 	message, err := bufio.NewReader(conn).ReadString('\n')
-	defer conn.Close()
 
 	if err != nil {
 		fmt.Println("error:", err)
 	}
 	fmt.Println("Message", message)
-	response, err := handleMessage(message)
+	err = handleMessage(message)
 
 	if err != nil {
 		fmt.Println("Error:", err)
 		conn.Write([]byte("Invalid RESP\n"))
 		return
 	}
-	fmt.Println("Response", response)
-
-	conn.Write([]byte(response))
+	go writeResponse(conn)
+	responseWg.Wait()
+	defer conn.Close()
 }
 
 func main() {
@@ -78,5 +105,4 @@ func main() {
 		fmt.Println("Calling handleConnection")
 		go handleConnection(conn)
 	}
-
 }
